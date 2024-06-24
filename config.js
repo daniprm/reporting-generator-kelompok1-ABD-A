cbe2d12d428e376d0ebe76a195de607c382e2097
Rahmat Ramadhan Permana <daniprmnn@gmail.com>
2024-06-24 14:50:41 +0700

    change server to localhost

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ export const sqlConfig = {
   user: "sa",
   password: "user123",
   database: "AdventureWorks2022",
-  server: "127.0.0.1",
+  server: "localhost",
   options: {
     encrypt: true,
     trustServerCertificate: true, // for local development
