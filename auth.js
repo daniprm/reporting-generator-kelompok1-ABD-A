33318959f34ceb6478c21ff3e47af03eb7de5eac
Rahmat Ramadhan Permana <daniprmnn@gmail.com>
2024-06-25 20:05:08 +0700

    update autentikasi

--- auth.js
+++ auth.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 import { secretKey, getConfigData } from "./config.js";
 
 export async function authenticateUser(user, password, database, server) {
-  if (getConfigData(user, password, database, server) === true) {
+  if ((await getConfigData(user, password, database, server)) === true) {
     const token = jwt.sign({ user }, secretKey, { expiresIn: "1h" });
     return token;
   }
